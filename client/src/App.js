40d02e0536f6cd67a7682c9b5a51a41457938d9f
sbrb <info.sbriju@gmail.com>
2023-03-09 18:40:24 +0530

    2

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
+import TextEditor from "./TextEditor";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App;
